4883ab9e1cbfbf253cbadb1069b4c5ee036aa8f9
Zbigniew Baranski <zb.baranski@icloud.com>
2019-10-25 15:19:42 +0200

    test

--- react/src/loadScripts.js
+++ react/src/loadScripts.js
@@ -1,15 +1,17 @@
+// for tests only
+
 const loadScripts = () => {
     const libs = [
         "/assets/libs/jquery/dist/jquery.min.js",
         "/assets/libs/@shopify/draggable/lib/es5/draggable.bundle.legacy.js",
-        "/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js",
+        "/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js", // menus
         "/assets/libs/autosize/dist/autosize.min.js",
         "/assets/libs/chart.js/dist/Chart.min.js",
         "/assets/libs/flatpickr/dist/flatpickr.min.js",
-        "/assets/libs/highlightjs/highlight.pack.min.js",
-        "/assets/libs/jquery-mask-plugin/dist/jquery.mask.min.js",
-        "/assets/libs/list.js/dist/list.min.js",
-        "/assets/libs/select2/dist/js/select2.min.js",
+        "/assets/libs/highlightjs/highlight.pack.min.js", // ?
+        "/assets/libs/jquery-mask-plugin/dist/jquery.mask.min.js", // ?
+        "/assets/libs/list.js/dist/list.min.js", // menus hide/sho
+        "/assets/libs/select2/dist/js/select2.min.js", // check
         "/assets/libs/chart.js/Chart.extension.min.js",
         `${process.env.PUBLIC_URL}/assets/js/theme.min.js`
       ];
