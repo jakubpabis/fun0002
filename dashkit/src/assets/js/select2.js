cc76440f61084c5d1e5faf450e161aeae5465d76
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-06-25 21:39:16 +0200

    testing paypal

--- dashkit/src/assets/js/select2.js
+++ dashkit/src/assets/js/select2.js
@@ -13,42 +13,41 @@
 
   var toggle = document.querySelectorAll('[data-toggle="select"]');
 
-
   //
   // Functions
   //
 
   function init(el) {
-    var elementOptions = el.dataset.options;
-        elementOptions = elementOptions ? JSON.parse(elementOptions) : {};
+    var elementOptions = el.dataset.options ? JSON.parse(el.dataset.options) : {};
+
     var defaultOptions = {
-      dropdownParent: $(el).closest('.modal').length ? $(el).closest('.modal') : $(document.body),
+      containerCssClass: el.getAttribute('class'),
+      dropdownAutoWidth: true,
+      dropdownCssClass: el.classList.contains('custom-select-sm') || el.classList.contains('form-control-sm') ? 'dropdown-menu dropdown-menu-sm show' : 'dropdown-menu show',
+      dropdownParent: el.closest('.modal') ? el.closest('.modal') : document.body,
       templateResult: formatTemplate
     };
-    var options = Object.assign(elementOptions, defaultOptions);
 
+    var options = Object.assign(defaultOptions, elementOptions);
+
+    // Init
     $(el).select2(options);
   }
 
   function formatTemplate(item) {
-    if (!item.id) {
+
+    // Quit if there's no avatar to display
+    if (!item.id || !item.element || !item.element.dataset.avatarSrc) {
       return item.text;
     }
 
-    var option = item.element;
-    var avatar = option.dataset.avatarSrc;
+    var avatar = item.element.dataset.avatarSrc;
+    var content = document.createElement('div');
 
-    if (avatar) {
-      var content = document.createElement('div');
-
-      content.innerHTML = '<span class="avatar avatar-xs mr-3"><img class="avatar-img rounded-circle" src="' + avatar + '" alt="' + item.text + '"></span><span>' + item.text + '</span>';
-    } else {
-      var content = item.text;
-    }
+    content.innerHTML = '<span class="avatar avatar-xs mr-3"><img class="avatar-img rounded-circle" src="' + avatar + '" alt="' + item.text + '"></span><span>' + item.text + '</span>';
 
     return content;
   }
-
 
   //
   // Events
@@ -59,5 +58,5 @@
       init(el);
     });
   }
-  
+
 })();
