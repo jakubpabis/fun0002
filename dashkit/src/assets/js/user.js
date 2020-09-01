0777addcd7d4bef185877f26b577fcd6f58b6db5
Jakub Pabis <jakubpabis@jakubs-macbook-pro.home>
2020-09-01 17:19:07 +0200

    add some scripts

---
+++ dashkit/src/assets/js/user.js
@@ -0,0 +1,36 @@
+function isScriptLoaded(url) {
+    var scripts = document.getElementsByTagName('script');
+    for (var i = scripts.length; i--;) {
+        if (scripts[i].src == url) return true;
+    }
+    return false;
+}
+
+function addScript($src)
+{
+	var head = document.getElementsByTagName('head')[0];
+	var script = document.createElement('script');
+	script.type = 'text/javascript';
+	script.src = $src;
+	head.appendChild(script);
+}
+
+function loadScript( url, callback ) {
+	var script = document.createElement( "script" )
+	script.type = "text/javascript";
+	if(script.readyState) {  // only required for IE <9
+		script.onreadystatechange = function() {
+			if ( script.readyState === "loaded" || script.readyState === "complete" ) {
+				script.onreadystatechange = null;
+				callback();
+			}
+		};
+	} else {  //Others
+		script.onload = function() {
+			callback();
+		};
+	}
+  
+	script.src = url;
+	document.getElementsByTagName( "head" )[0].appendChild( script );
+}
